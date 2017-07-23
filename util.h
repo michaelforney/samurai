25fdfe0e0dec63c37ddbf3d6dd53854def9b807f
Michael Forney <mforney@mforney.org>
2017-07-23 00:25:19 -0700

    Describe mkstr and canonpath in util.h

--- util.h
+++ util.h
@@ -15,6 +15,10 @@ void *xrealloc(void *, size_t);
 void *xcalloc(size_t, size_t);
 char *xstrdup(const char *, size_t);
 
-struct string *mkstr(size_t);
+/* allocates a new string with length n. n + 1 bytes are allocated for
+ * s, but not initialized. */
+struct string *mkstr(size_t n);
 
+/* canonicalizes the given path by removing duplicate slashes, and
+ * folding '/.' and 'foo/..' */
 void canonpath(struct string *);
