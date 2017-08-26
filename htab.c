a602944e8b81aa6f84cf469900248fe0272babbf
Michael Forney <mforney@mforney.org>
2017-08-26 12:16:33 -0700

    htab: Add fallthrough comments to silence GCC 7 with -Wextra

--- htab.c
+++ htab.c
@@ -178,12 +178,12 @@ murmurhash64a(void *ptr, size_t len)
 	}
 
 	switch (len & 0x7) {
-	case 7: h ^= (uint64_t)p[6] << 48;
-	case 6: h ^= (uint64_t)p[5] << 40;
-	case 5: h ^= (uint64_t)p[4] << 32;
-	case 4: h ^= (uint64_t)p[3] << 24;
-	case 3: h ^= (uint64_t)p[2] << 16;
-	case 2: h ^= (uint64_t)p[1] << 8;
+	case 7: h ^= (uint64_t)p[6] << 48;  /* fallthrough */
+	case 6: h ^= (uint64_t)p[5] << 40;  /* fallthrough */
+	case 5: h ^= (uint64_t)p[4] << 32;  /* fallthrough */
+	case 4: h ^= (uint64_t)p[3] << 24;  /* fallthrough */
+	case 3: h ^= (uint64_t)p[2] << 16;  /* fallthrough */
+	case 2: h ^= (uint64_t)p[1] <<  8;  /* fallthrough */
 	case 1: h ^= (uint64_t)p[0];
 		h *= m;
 	}
