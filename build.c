michaelforney/samurai

commit 3bc132f0677786aec525b0fa0f28835752124fbe

build: Move options into a structure

build.c
@@ -24,6 +24,7 @@ struct job {
 	bool failed;
 };
 
+struct buildoptions buildopts = {.maxfail = 1};
 static struct edge *work;
 static size_t nstarted, ntotal;
 static bool consoleused;
@@ -148,7 +149,7 @@ buildadd(struct node *n)
 }
 
 static int
-jobstart(struct job *j, struct edge *e, bool verbose)
+jobstart(struct job *j, struct edge *e)
 {
 	size_t i;
 	struct node *n;
@@ -186,7 +187,7 @@ jobstart(struct job *j, struct edge *e, bool verbose)
 	argv[2] = j->cmd->s;
 
 	if (!consoleused) {
-		description = verbose ? NULL : edgevar(e, "description");
+		description = buildopts.verbose ? NULL : edgevar(e, "description");
 		if (!description || description->n == 0)
 			description = j->cmd;
 		printf("[%zu/%zu] %s\n", nstarted, ntotal, description->s);
@@ -403,15 +404,15 @@ done:
 }
 
 void
-build(int maxjobs, int maxfail, bool verbose)
+build(void)
 {
-	struct job jobs[maxjobs];
-	struct pollfd fds[maxjobs];
+	struct job jobs[buildopts.maxjobs];
+	struct pollfd fds[buildopts.maxjobs];
 	size_t i;
-	int avail[maxjobs], j, next = 0, numjobs = 0, numfail = 0;
+	int avail[buildopts.maxjobs], j, next = 0, numjobs = 0, numfail = 0;
 	struct edge *e;
 
-	for (j = 0; j < maxjobs; ++j) {
+	for (j = 0; j < buildopts.maxjobs; ++j) {
 		jobs[j].buf.data = NULL;
 		jobs[j].buf.len = 0;
 		jobs[j].buf.cap = 0;
@@ -419,7 +420,7 @@ build(int maxjobs, int maxfail, bool verbose)
 		fds[j].events = POLLIN;
 		avail[j] = j + 1;
 	}
-	avail[maxjobs - 1] = -1;
+	avail[buildopts.maxjobs - 1] = -1;
 
 	if (!work)
 		warnx("nothing to do");
@@ -427,7 +428,7 @@ build(int maxjobs, int maxfail, bool verbose)
 	nstarted = 0;
 	for (;;) {
 		/* start ready edges */
-		while (work && numjobs < maxjobs && (!maxfail || numfail < maxfail)) {
+		while (work && numjobs != buildopts.maxjobs && numfail != buildopts.maxfail) {
 			e = work;
 			work = work->worknext;
 			if (e->rule == &phonyrule) {
@@ -435,7 +436,7 @@ build(int maxjobs, int maxfail, bool verbose)
 					nodedone(e->out[i], false);
 				continue;
 			}
-			fds[next].fd = jobstart(&jobs[next], e, verbose);
+			fds[next].fd = jobstart(&jobs[next], e);
 			if (fds[next].fd < 0) {
 				warnx("job failed to start");
 				++numfail;
@@ -449,9 +450,9 @@ build(int maxjobs, int maxfail, bool verbose)
 
 		/* wait for job to finish */
 		do {
-			if (poll(fds, maxjobs, -1) < 0)
+			if (poll(fds, buildopts.maxjobs, -1) < 0)
 				err(1, "poll");
-			for (j = 0; j < maxjobs; ++j) {
+			for (j = 0; j < buildopts.maxjobs; ++j) {
 				if (!fds[j].revents || jobwork(&jobs[j]))
 					continue;
 				--numjobs;
@@ -461,12 +462,12 @@ build(int maxjobs, int maxfail, bool verbose)
 				if (jobs[j].failed)
 					++numfail;
 			}
-		} while (numjobs == maxjobs);
+		} while (numjobs == buildopts.maxjobs);
 	}
-	for (j = 0; j < maxjobs; ++j)
+	for (j = 0; j < buildopts.maxjobs; ++j)
 		free(jobs[j].buf.data);
 	if (numfail > 0) {
-		if (numfail < maxfail)
+		if (numfail < buildopts.maxfail)
 			errx(1, "cannot make progress due to previous errors");
 		else if (numfail > 1)
 			errx(1, "subcommands failed");

samurai.c
@@ -76,12 +76,8 @@ builddefault(void)
 int
 main(int argc, char *argv[])
 {
-	/* options */
 	char *manifest = "build.ninja";
-	int maxjobs = 0, maxfail = 1;
 	struct tool *tool = NULL;
-	bool verbose = false;
-
 	struct node *n;
 	int builddirfd, tries;
 	char *end;
@@ -97,44 +93,43 @@ main(int argc, char *argv[])
 		manifest = EARGF(usage());
 		break;
 	case 'j':
-		maxjobs = strtol(EARGF(usage()), NULL, 10);
-		if (maxjobs <= 0)
+		buildopts.maxjobs = strtol(EARGF(usage()), NULL, 10);
+		if (buildopts.maxjobs <= 0)
 			errx(1, "invalid -j parameter");
 		break;
 	case 'k':
-		maxfail = strtol(EARGF(usage()), &end, 10);
+		buildopts.maxfail = strtol(EARGF(usage()), &end, 10);
 		if (*end)
 			errx(1, "invalid -k parameter");
-		if (maxfail < 0)
-			maxfail = 0;
+		if (buildopts.maxfail == 0)
+			buildopts.maxfail = -1;
 		break;
 	case 't':
 		tool = toolget(EARGF(usage()));
 		goto argdone;
 	case 'v':
-		verbose = true;
+		buildopts.verbose = true;
 		break;
 	default:
 		usage();
 	} ARGEND
 argdone:
-
-	if (!maxjobs) {
+	if (!buildopts.maxjobs) {
 #ifdef _SC_NPROCESSORS_ONLN
 		int n = sysconf(_SC_NPROCESSORS_ONLN);
 		switch (n) {
 		case -1: case 0: case 1:
-			maxjobs = 2;
+			buildopts.maxjobs = 2;
 			break;
 		case 2:
-			maxjobs = 3;
+			buildopts.maxjobs = 3;
 			break;
 		default:
-			maxjobs = n + 2;
+			buildopts.maxjobs = n + 2;
 			break;
 		}
 #else
-		maxjobs = 2;
+		buildopts.maxjobs = 2;
 #endif
 	}
 
@@ -165,7 +160,7 @@ retry:
 	if (n && n->gen) {
 		buildadd(n);
 		if (n->dirty) {
-			build(maxjobs, maxfail, verbose);
+			build();
 			if (++tries > 100)
 				errx(1, "manifest '%s' dirty after 100 tries", manifest);
 			goto retry;
@@ -183,7 +178,7 @@ retry:
 	} else {
 		builddefault();
 	}
-	build(maxjobs, maxfail, verbose);
+	build();
 	logclose();
 
 	return 0;

build.h
@@ -1,6 +1,13 @@
 struct node;
 
+struct buildoptions {
+	int maxjobs, maxfail;
+	_Bool verbose;
+};
+
+extern struct buildoptions buildopts;
+
 /* schedule a particular target to be built */
 void buildadd(struct node *);
 /* execute rules to build the scheduled targets */
-void build(int maxjobs, int maxfail, _Bool verbose);
+void build(void);

commit c841e765f2f35871d6ce5cdd02dcde080e73694b

Fix maxfail when multiple jobs fail at the same time
This was accidentally broken in 3bc132f0677786aec525b0fa0f28835752124fbe.

build.c
@@ -457,7 +457,7 @@ build(void)
 	nstarted = 0;
 	for (;;) {
 		/* start ready edges */
-		while (work && numjobs != buildopts.maxjobs && numfail != buildopts.maxfail) {
+		while (work && numjobs < buildopts.maxjobs && numfail < buildopts.maxfail) {
 			e = work;
 			work = work->worknext;
 			if (e->rule == &phonyrule) {

samurai.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 200809L
 #include <errno.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -114,8 +115,8 @@ main(int argc, char *argv[])
 		buildopts.maxfail = strtol(EARGF(usage()), &end, 10);
 		if (*end)
 			errx(1, "invalid -k parameter");
-		if (buildopts.maxfail == 0)
-			buildopts.maxfail = -1;
+		if (buildopts.maxfail <= 0)
+			buildopts.maxfail = INT_MAX;
 		break;
 	case 't':
 		tool = toolget(EARGF(usage()));