michaelforney/samurai

commit d6c8545ca87e6b063ef57a8ba34b9706cc1aa3a7

build: Generator with no entry in build log is not dirty
This matches ninja's behavior and prevents a re-run of the ninja
generation script during the first build.

build.c
@@ -58,11 +58,6 @@ isdirty(struct node *n, struct node *newest, bool generator, bool restat)
 			warnx("explain %s: missing", n->path->s);
 		return true;
 	}
-	if (n->logmtime == MTIME_MISSING) {
-		if (buildopts.explain)
-			warnx("explain %s: no record in .ninja_log", n->path->s);
-		return true;
-	}
 	if (isnewer(newest, n) && !restat) {
 		if (buildopts.explain) {
 			warnx("explain %s: older than input '%s': %" PRId64 " vs %" PRId64,
@@ -70,7 +65,13 @@ isdirty(struct node *n, struct node *newest, bool generator, bool restat)
 		}
 		return true;
 	}
-	if (newest && n->logmtime < newest->mtime / 1000000000) {
+	if (n->logmtime == MTIME_MISSING) {
+		if (!generator) {
+			if (buildopts.explain)
+				warnx("explain %s: no record in .ninja_log", n->path->s);
+			return true;
+		}
+	} else if (newest && n->logmtime < newest->mtime / 1000000000) {
 		if (buildopts.explain) {
 			warnx("explain %s: recorded mtime is older than input '%s': %" PRId64 " vs %" PRId64,
 			      n->path->s, newest->path->s, n->logmtime, newest->mtime / 1000000000);

commit c7b530bfeeda90a681e1184d94417a4ced637bed

build: Fix isdirty for restat=1, generator=1, and no build log entry
This was accidentally broken in d6c8545ca8, which fixed isdirty for
generator=1 and no build log, but did not consider restat=1. The
"older than input" check was skipped for restat=1 because a newer
log entry can override an old output (due to restat detecting no
change). Instead, it relied on the "missing log entry" check.
However, when d6c8545ca8 fixed that check to ignore generator
actions, it had the side effect of failing to mark actions with
restat and generator as dirty even if the output was older than the
newest input.

build.c
@@ -58,7 +58,7 @@ isdirty(struct node *n, struct node *newest, bool generator, bool restat)
 			warnx("explain %s: missing", n->path->s);
 		return true;
 	}
-	if (isnewer(newest, n) && !restat) {
+	if (isnewer(newest, n) && (!restat || n->logmtime == MTIME_MISSING)) {
 		if (buildopts.explain) {
 			warnx("explain %s: older than input '%s': %" PRId64 " vs %" PRId64,
 			      n->path->s, newest->path->s, n->mtime, newest->mtime);