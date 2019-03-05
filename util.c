13e6bd221731d7409750c84cc3f9bfc2f9803945
Michael Forney <mforney@mforney.org>
2019-03-04 18:38:30 -0800

    Fix error message printing

--- util.c
+++ util.c
@@ -18,7 +18,7 @@ vwarn(const char *fmt, va_list ap)
 {
 	fprintf(stderr, "%s: ", argv0);
 	vfprintf(stderr, fmt, ap);
-	if (fmt[0] && fmt[strlen(fmt)] == ':') {
+	if (fmt[0] && fmt[strlen(fmt) - 1] == ':') {
 		putc(' ', stderr);
 		perror(NULL);
 	} else {
