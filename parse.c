f549b757394fdb4ac27e4a4bc42e865e98434361
Michael Forney <mforney@mforney.org>
2020-01-22 11:36:30 -0800

    parse: Check that rules have $rspfile iff they have $rspfile_content

--- parse.c
+++ parse.c
@@ -35,7 +35,7 @@ parserule(struct scanner *s, struct environment *env)
 	struct rule *r;
 	char *var;
 	struct evalstring *val;
-	bool hascommand = false;
+	bool hascommand = false, hasrspfile = false, hasrspcontent = false;
 
 	r = mkrule(scanname(s));
 	scannewline(s);
@@ -45,9 +45,15 @@ parserule(struct scanner *s, struct environment *env)
 		ruleaddvar(r, var, val);
 		if (strcmp(var, "command") == 0)
 			hascommand = true;
+		else if (strcmp(var, "rspfile") == 0)
+			hasrspfile = true;
+		else if (strcmp(var, "rspfile_content") == 0)
+			hasrspcontent = true;
 	}
 	if (!hascommand)
 		fatal("rule '%s' has no command", r->name);
+	if (hasrspfile != hasrspcontent)
+		fatal("rule '%s' has rspfile and no rspfile_content or vice versa", r->name);
 	envaddrule(env, r);
 }
 
